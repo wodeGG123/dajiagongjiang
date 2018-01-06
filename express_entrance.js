0da2cda938aa031c5e9be493b57240c48935b7d1
panda.cn <65720230@qq.com>
2018-01-06 11:08:22 +0800

    express

---
+++ express_entrance.js
@@ -0,0 +1,20 @@
+//express_demo.js 文件
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+app.use(express.static(path.join(__dirname, '/')));
+app.get('/', function (req, res) {
+   // res.send('Hello World');
+   res.sendFile(__dirname + '/index.html');
+})
+
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
